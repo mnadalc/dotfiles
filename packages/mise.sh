ee3d8226d51a4fa09dc585bf21833381c5dc8036
Miguel Nadal <11161738+mnadalc@users.noreply.github.com>
2026-02-06 10:11:01 +0100

    fix(mise): install from config directory instead of config file

--- packages/mise.sh
+++ packages/mise.sh
@@ -1,11 +1,12 @@
 source './osx/utils.sh'
 
-MISE_CONFIG="$HOME/.dotfiles/.config/mise/config.toml"
+MISE_DIR="$HOME/.dotfiles/.config/mise"
+MISE_CONFIG="$MISE_DIR/config.toml"
 
 install_mise () {
   print_info "Trusting and installing mise"
-  mise trust $MISE_CONFIG
-  cd $MISE_CONFIG
+  mise trust "$MISE_CONFIG"
+  cd "$MISE_DIR"
   mise install
   mise ls
   print_success "mise installed"
